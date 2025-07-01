6d8bd6b900140724778cf8b202e7313f644804ad
zgt <matthewcalayo@gmail.com>
2025-07-01 15:33:15 -0400

    fixed typing error

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Toaster as Sonner, ToasterProps } from "sonner"
+import { Toaster as Sonner, type ToasterProps } from "sonner"
 
 const Toaster = ({ ...props }: ToasterProps) => {
   return (
