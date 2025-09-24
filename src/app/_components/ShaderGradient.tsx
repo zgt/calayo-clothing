8e9e69f7ea54c74ddfa8348348c6081212184c36
Matt Calayo <matthewcalayo@gmail.com>
2025-09-24 17:26:32 -0400

    fixed shader

--- src/app/_components/ShaderGradient.tsx
+++ src/app/_components/ShaderGradient.tsx
@@ -16,8 +16,31 @@ export default function ShaderGradientBackground() {
         }}
       >
         <ShaderGradient
-          control="query"
-          urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23003d00&bgColor2=%23003d00&brightness=0.9&cAzimuthAngle=180&cDistance=11.9&cPolarAngle=120&cameraZoom=15.6&color1=%231a7833&color2=%23065f2c&color3=%23005800&destination=onCanvas&embedMode=off&envPreset=lobby&format=gif&fov=50&frameRate=10&gizmoHelper=hide&grain=on&lightType=3d&pixelDensity=2.7&positionX=0&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.4&rotationX=-50&rotationY=0&rotationZ=70&shader=positionMix&toggleAxis=false&type=sphere&uAmplitude=1.2&uDensity=2.6&uFrequency=5.5&uSpeed=0.1&uStrength=3.2&uTime=7.4&wireframe=false&zoomOut=false"
+          type="waterPlane"
+          //cameraZoom={2}
+          cDistance={4}
+          cPolarAngle={125}
+          cAzimuthAngle={180}
+          positionX={0}
+          positionY={0}
+          positionZ={0}
+          rotationX={-50}
+          rotationY={0}
+          rotationZ={70}
+          animate="on"
+          grain="on"
+          //shader="none"
+          color1="#1a7833"
+          color2="#065f2c"
+          color3="#005800"
+          uSpeed={0.1}
+          uStrength={3}
+          uDensity={2.7}
+          uFrequency={0.5}
+          uAmplitude={1.5}
+          reflection={0.5}
+          lightType="3d"
+          brightness={1}
         />
       </ShaderGradientCanvas>
     </div>
